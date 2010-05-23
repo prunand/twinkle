5b7c9ea36813b3d28742745b0bf832b7dbc867a0
Andrew Prunicki <prunand@iit.edu>
2010-05-23 15:00:00 -0500

    Added cleanup of dead listeners when adding or removing listeners.

--- src/src/com/prunicki/suzuki/twinkle/Utils.java
+++ src/src/com/prunicki/suzuki/twinkle/Utils.java
@@ -3,38 +3,51 @@ package com.prunicki.suzuki.twinkle;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Utils {
-    
     public static void addPropertyChangeListener(List<WeakReference<PropertyChangeListener>> listeners, PropertyChangeListener listener) {
         boolean add = true;
+        ArrayList<WeakReference<PropertyChangeListener>> deadListeners = null;
         
+        PropertyChangeListener currentListener = null;
         for (WeakReference<PropertyChangeListener> listenerRef : listeners) {
-            if (listenerRef.get() == listener) {
+            currentListener = listenerRef.get();
+            if (currentListener == listener) {
                 add = false;
                 break;
+            } else if (currentListener == null) {
+                if (deadListeners == null) {
+                    deadListeners = new ArrayList<WeakReference<PropertyChangeListener>>();
+                }
+                deadListeners.add(listenerRef);
             }
         }
         
+        if (deadListeners != null) {
+            removeDeadListeners(listeners, deadListeners);
+        }
         if (add) {
             listeners.add(new WeakReference<PropertyChangeListener>(listener));
         }
     }
     
     public static void removePropertyChangeListener(List<WeakReference<PropertyChangeListener>> listeners, PropertyChangeListener listener) {
-        int idx = -1;
+        ArrayList<WeakReference<PropertyChangeListener>> deadListeners = new ArrayList<WeakReference<PropertyChangeListener>>();
         
-        for (int i = 0; i < listeners.size(); i++) {
-            if (listeners.get(i).get() == listener) {
-                idx = i;
+        PropertyChangeListener currentListener = null;
+        for (WeakReference<PropertyChangeListener> listenerRef : listeners) {
+            currentListener = listenerRef.get();
+            if (currentListener == listener) {
+                deadListeners.add(listenerRef);
                 break;
+            } else if (currentListener == null) {
+                deadListeners.add(listenerRef);
             }
         }
         
-        if (idx >= 0) {
-            listeners.remove(idx);
-        }
+        removeDeadListeners(listeners, deadListeners);
     }
 
     public static final void firePropertyChangeEvent(List<WeakReference<PropertyChangeListener>> listeners,
@@ -46,6 +59,13 @@ public final class Utils {
             if (listener != null) {
                 listener.propertyChange(event);
             }
+        }
+    }
+    
+    private static void removeDeadListeners(List<WeakReference<PropertyChangeListener>> listeners,
+            List<WeakReference<PropertyChangeListener>> deadListeners) {
+        for (WeakReference<PropertyChangeListener> listenerRef : deadListeners) {
+            listeners.remove(listenerRef);
         }
     }
 }
