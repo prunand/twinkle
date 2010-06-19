f3defad6e2e0bbc7633b4059c97d724feb4d4430
Andrew Prunicki <prunand@iit.edu>
2010-06-18 21:49:29 -0500

    Added upgrade and uninstall.

--- src/src/com/prunicki/suzuki/twinkle/Main.java
+++ src/src/com/prunicki/suzuki/twinkle/Main.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -35,11 +36,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.prunicki.suzuki.twinkle.db.SuzukiDAO;
 
 public class Main extends TwinkleActivity {
     public static final String TAG = "SuzukiTwinkle";
+    
+    private static final String TWINKLE_PKG_NAME = "com.prunicki.twinkle";
+    private static final String SUZUKI_TWINKLE_PKG_NAME = "com.prunicki.suzuki.twinkle";
+    private static final String ANDROID_SETTINGS_PKG = "com.android.settings";
+    private static final String ANDROID_SETTINGS_APP_PKG_NAME = ANDROID_SETTINGS_PKG + ".ApplicationPkgName";
+    private static final String INSTALL_APP_DETAILS = ANDROID_SETTINGS_PKG + ".InstalledAppDetails";
     
     private Button mNameButton;
     private Button mPitchButton;
@@ -67,12 +75,9 @@ public class Main extends TwinkleActivity {
     protected void onResume() {
         super.onResume();
         
-        Intent twinkleIntent = new Intent();
-        twinkleIntent.setClassName("com.prunicki.twinkle", "com.prunicki.twinkle.Main");
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> queryIntentActivities = pm.queryIntentActivities(twinkleIntent, PackageManager.MATCH_DEFAULT_ONLY);
-        int size = queryIntentActivities.size();
-        if (size == 0) {
+        if (twinkleInstalled()) {
+            showUninstallDialog();
+        } else {
             SuzukiDAO dao = app.getDAO();
 
             int numDismisses = dao.queryNumberOfDismisses();
@@ -105,28 +110,41 @@ public class Main extends TwinkleActivity {
                 
                 LinearLayout layout = (LinearLayout) d.findViewById(R.id.AboutLayout);
                 layout.setOnClickListener(new AboutListener(d));
+                View upgradeView = d.findViewById(R.id.Upgrade);
+                if (twinkleInstalled()) {
+                    upgradeView.setVisibility(View.GONE);
+                } else {
+                    upgradeView.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!startMarket(TWINKLE_PKG_NAME)) {
+                                showNoMarketToast();
+                            }
+                        }
+                    });
+                }
                 
-                String appName = Main.this.getResources().getString(R.string.app_name);
+                String appName = getString(R.string.app_name);
                 d.setTitle("About " + appName);
                 d.show();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
     private void showUpgradeDialog() {
         AlertDialog.Builder dlgBldr = new AlertDialog.Builder(this);
-        dlgBldr.setTitle("Upgrade");
-        dlgBldr.setMessage("Suzuki Twinkle is now \"Twinkle\".\n" +
-                "Please install Twinkle from the Android Market.");
+        dlgBldr.setTitle("Upgrade to Twinkle");
+        dlgBldr.setMessage("Suzuki Twinkle is now \"Twinkle\".\n\n" +
+            "Would you like to upgrade to Twinkle now?");
         
-        dlgBldr.setPositiveButton("Install Now", new DialogInterface.OnClickListener() {
+        dlgBldr.setPositiveButton("Upgrade Now", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Uri marketUri = Uri.parse("market://search?q=pname:com.prunicki.twinkle");
-                Intent marketIntent =  new Intent(Intent.ACTION_VIEW).setData(marketUri);
-                startActivity(marketIntent);
-                dialog.dismiss();
+                if (!startMarket(TWINKLE_PKG_NAME)) {
+                    showNoMarketToast();
+                }
+                finish();
             }
         });
         dlgBldr.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
@@ -138,6 +156,66 @@ public class Main extends TwinkleActivity {
         
         AlertDialog dlg = dlgBldr.create();
         dlg.show();
+    }
+    
+    private void showUninstallDialog() {
+        AlertDialog.Builder dlgBldr = new AlertDialog.Builder(this);
+        dlgBldr.setTitle("Uninstall Suzuki Twinkle");
+        dlgBldr.setMessage("Twinkle has replaced Suzuki Twinkle and you have it.\n\n" +
+            "Would you like to uninstall Suzuki Twinkle now?");
+        
+        dlgBldr.setPositiveButton("Uninstall Now", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (!startMarket(SUZUKI_TWINKLE_PKG_NAME)) {
+                    startManageAppActivity();
+                }
+
+                finish();
+            }
+        });
+        dlgBldr.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        
+        AlertDialog dlg = dlgBldr.create();
+        dlg.show();
+    }
+    
+    private boolean twinkleInstalled() {
+        Intent twinkleIntent = new Intent();
+        twinkleIntent.setClassName(TWINKLE_PKG_NAME, TWINKLE_PKG_NAME + ".Main");
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> intentActivities = pm.queryIntentActivities(twinkleIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        int size = intentActivities.size();
+        return size > 0;
+    }
+    
+    void startManageAppActivity() {
+        Intent intent = new Intent();
+        intent.setClassName(ANDROID_SETTINGS_PKG, INSTALL_APP_DETAILS);
+        intent.putExtra(ANDROID_SETTINGS_APP_PKG_NAME, SUZUKI_TWINKLE_PKG_NAME);
+        startActivity(intent);
+    }
+    
+    boolean startMarket(String pkgName) {
+        boolean started = true;
+        Uri marketUri = Uri.parse("market://search?q=pname:" + pkgName);
+        Intent marketIntent =  new Intent(Intent.ACTION_VIEW).setData(marketUri);
+        try {
+            startActivity(marketIntent);
+        } catch (ActivityNotFoundException e) {
+            started = false;
+        }
+        return started;
+    }
+
+    void showNoMarketToast() {
+        Toast toast = Toast.makeText(Main.this, "Android Market not found!", Toast.LENGTH_LONG);
+        toast.show();
     }
 
     private OnClickListener mPitchListener = new OnClickListener() {
@@ -163,7 +241,6 @@ public class Main extends TwinkleActivity {
     };
 
     private class AboutListener implements OnClickListener {
-        
         private Dialog mDialog;
         
         public AboutListener(Dialog dialog) {
